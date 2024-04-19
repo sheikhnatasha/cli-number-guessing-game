694d480341191aacffb1cac72752a1ce09258ee3
Sheikh Natasha Arif <sheikhnatasha075@gmail.com>
2024-04-19 22:56:41 +0500

    adding a number guessing game

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\twellcome to itxnatashaarif - cli number guessing game\n");
+const rendomnumber = Math.floor(Math.random() * 5 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userguessenumber",
+        type: "number",
+        message: "enter your guess number(number limit from 1 to 5):",
+    },
+]);
+if (answers.userguessenumber === rendomnumber) {
+    console.log("congratulations ! you guess a correct number. and you won a bycycle");
+}
+else {
+    console.log("sorry, you guess a wrong number");
+}
